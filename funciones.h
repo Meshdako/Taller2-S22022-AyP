efd9ffd9c5323a989421dbfc86df4d656fa70a09
Meshdako <msobarzo@utem.cl>
2022-10-11 00:37:56 -0300

    archivo declarativo de las funciones creado y con las funciones escritas.

---
+++ funciones.h
@@ -0,0 +1,19 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+//Función que determine si un número natural es descuadrado. 
+int Descuadrado(int num);
+
+//Función que determine si un número natural es reparado.
+int Reparado(int num);
+
+//Función que cree un arreglo con los primeros n ≤ 20 números descuadrados. 10
+void Descuadrados(int *num_Desc[]);
+
+//Función que cree un arreglo con los primeros n ≤ 20 números reparados.
+void Reparados(int *num_Rep[]);
+
+//Función que despliegue en pantalla los n elementos un arreglo. Usarla para ver los dos arreglos creados. 
+void Imprimir(int arreglo_num[]);
+
+#endif  //FUNCIONES_h
