c1572f1ff531a6068d4e940793a44a168bd70047
Meshdako <msobarzo@utem.cl>
2022-10-11 00:26:55 -0300

    Archivo main creado

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * @brief 
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+
+int main(int argc, char **argv)
+{
+    printf("Hello World!\n");
+
+    return 0;
+}
