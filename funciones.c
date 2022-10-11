cea94b0503cef4f964fb51e334b01d1532c5c4ff
Meshdako <msobarzo@utem.cl>
2022-10-11 01:42:39 -0300

    Agregada funciones básicas: ult_dig, pri_dig, pri_2_dig, es par

---
+++ funciones.c
@@ -0,0 +1,65 @@
+#include "funciones.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int Ult_Dig(int num)
+{
+    return num % 10;
+}
+
+int Pri_Dig(int num)
+{
+    while(num > 10)
+        num = num / 10;
+
+    return num;
+}
+
+int Pri_2_Dig(int num)
+{
+    while(num > 100)
+        num = num / 10;
+    
+    return num;
+}
+
+int Es_Par(int num)
+{
+    if(num % 2 == 0)
+        return 1;
+    else
+        return 0;
+}
+
+int Descuadrado(int num)
+{
+    int ult = Ult_Dig(num);
+    if( Pri_Dig(num) == (ult * ult) || Pri_2_Dig(num) == (ult * ult) )
+        return 1;
+    else
+        return 0;
+}
+
+int Reparado(int num)
+{
+    if( (Es_Par(num) == 1) && (Es_Par(Pri_Dig(num)) == 0) )
+        return 1;
+    else
+        return 0;
+}
+
+void Descuadrados(int *num_Desc[])
+{
+
+}
+
+void Reparados(int *num_Rep[])
+{
+
+}
+
+void Imprimir(int arreglo_num[])
+{
+
+}
