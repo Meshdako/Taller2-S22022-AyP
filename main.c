b2edd8c5ad22d199c18ebc44b552da1c6e45d684
Meshdako <msobarzo@utem.cl>
2022-10-11 01:55:06 -0300

    Menú de llamada establecido

--- main.c
+++ main.c
@@ -15,12 +15,38 @@
 
 int main(int argc, char **argv)
 {
-    if(argc > 1){
-        printf("Hello World!\n");
-        printf("Es 75416 un número Reparado?: %d", Reparado(25416));        
-    }
-    else{
-        printf("Bye\n");
-    }
+    //if(argc > 1){
+        printf("Hello AyP!\n");
+
+        int indice = 20;
+
+        //Arreglo de números descuadrados y reparados.
+        int Array_Desc[indice];
+        int Array_Rep[indice];
+
+        printf("\nSe calcularán los primeros 20 números Descuadrados.");   
+        printf("\nEstos se guardarán en un arreglo de enteros.");
+
+        /* Llamado a la función Descuadrados */
+        Descuadrados(&Array_Desc);
+
+        printf("\nAhora, se calcularán los primeros 20 números Reparados.");   
+        printf("\nEstos se guardarán en un arreglo de enteros.");
+
+        /* Llamado a la función Reparados */
+        Reparados(&Array_Rep);
+
+        printf("\nAhora procederemos a imprimir los dos arreglos.");
+        printf("\nNúmeros descuadrados:");
+
+        Imprimir(Array_Desc, indice);
+
+        printf("\nNúmeros reparados:");
+        
+        Imprimir(Array_Rep, indice);
+    //}
+    //else{
+        //printf("Bye\n");
+    //}
     return 0;
 }
