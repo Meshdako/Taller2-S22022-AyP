6d431ab2bae6399066aa127886731764449836e7
Meshdako <msobarzo@utem.cl>
2022-10-12 14:44:30 -0300

    Corregida la llamada a las funciones descuadrados y reparados.

--- main.c
+++ main.c
@@ -16,34 +16,24 @@
 int main(int argc, char **argv)
 {
     //if(argc > 1){
+        system("clear");
         printf("Hello AyP!\n");
-
-        int indice = 20;
-
+        
         //Arreglo de números descuadrados y reparados.
-        int Array_Desc[indice];
-        int Array_Rep[indice];
-
-        printf("\nSe calcularán los primeros 20 números Descuadrados.");   
-        printf("\nEstos se guardarán en un arreglo de enteros.");
+        int Array_Desc[20];
+        int Array_Rep[20];
 
         /* Llamado a la función Descuadrados */
-        Descuadrados(&Array_Desc);
+        Descuadrados(Array_Desc);
 
-        printf("\nAhora, se calcularán los primeros 20 números Reparados.");   
-        printf("\nEstos se guardarán en un arreglo de enteros.");
-
-        /* Llamado a la función Reparados */
-        Reparados(&Array_Rep);
-
-        printf("\nAhora procederemos a imprimir los dos arreglos.");
-        printf("\nNúmeros descuadrados:");
-
-        Imprimir(Array_Desc, indice);
-
-        printf("\nNúmeros reparados:");
+        Reparados(Array_Rep);
         
-        Imprimir(Array_Rep, indice);
+        printf("\n\tNúmeros descuadrados.");
+        Imprimir(Array_Desc);
+        
+
+        printf("\n\tNúmeros reparados.");
+        Imprimir(Array_Rep);
     //}
     //else{
         //printf("Bye\n");
